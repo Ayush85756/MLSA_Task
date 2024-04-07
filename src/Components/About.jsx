c187e90ed452edfc9072c128c24dc13a952f5617
Ayush Yaduvanshi <ayush85756@gmail.com>
2024-04-08 02:01:05 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/photo.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "green leafs background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Web developer and currently a student studying at Gla University and I am always very excited to learn new things and vey happy to use in real life implementation .";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web Developer",
+  "Software developer",
+  "Web designer",
+  "CyberSecurity ",
+  "App designer",
+  "Gaming",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am highly motivated software developer. always ready to contribute in real life practical problems and use my problem solving skills to contribute towards humanity";
 
 const About = () => {
   return (
